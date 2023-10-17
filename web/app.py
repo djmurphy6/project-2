3078c682baef135c37baf26a21eeeea0ffcb4ab0
Dylan Murphy <dmurphy6@uoregon.edu>
2023-10-16 18:40:35 -0700

    partially there with the file checking functionality, still not fully working

--- web/app.py
+++ web/app.py
@@ -2,7 +2,7 @@
 Dylan Murphy's Flask API.
 """
 
-from flask import Flask
+from flask import Flask, abort, send_from_directory
 import os
 import configparser
 
@@ -33,9 +33,35 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "UOCIS docker demo!\n"
-@app.route("/trivia.html")
-def trivia():
-    return "Tar Pit Surf Co.\n"
+
+
+@app.route("/<string:pName>")
+def pathname(pName):
+        #loop through to look for forbidden characters
+        illegal = 0
+        for i in range(len(pName)):  # for loop to go through each character
+            if pName[i] == '~':
+                illegal += 1
+            if pName[i] == '.' and (i < len(pName) and pName[i+1] == '.'): # check for '..'
+                illegal += 1
+                break
+            #if illegal transmit error code 403
+            if illegal:
+                abort(403)
+            else:
+                #If none found, see if the address entered has a corresponding file
+                path = "./pages" + pName
+                if os.path.exists(path):
+                    return send_from_directory('pages/', pName), 200
+                else:
+                    abort(404)
+
+@app.errorhandler(403)
+def forbidden(e):
+   return send_from_directory('pages/', '403.html'), 403
+@app.errorhandler(404)
+def page_not_found(e):
+    return send_from_directory('pages/', '404.html'), 404       
 
 
 if __name__ == "__main__":
